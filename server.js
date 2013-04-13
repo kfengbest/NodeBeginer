043e0fabe4f52e2e3d51fcfc9ef6080be5fbf8e2
fengka <kaven.feng@autodesk.com>
2013-04-13 10:07:00 +0800

    demoed how to parse URL

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var http = require("http");
+var url = require("url");
+
 
 function start(){
 	function onRequest(request, response){
 		console.log("request received...");
+		var pathname = url.parse(request.url).pathname;
+		console.log(request.url);
+
 		response.writeHead(200,{"Content-Type":"text/plain"});
-		response.write("Hello world");
+		response.write("Hello node.js");
+		response.write("\n url:" + request.url);
+		response.write("\n pathname:" + pathname);
 		response.end();
 	}
 	
